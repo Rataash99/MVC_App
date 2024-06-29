c611740fae83c188de1b655f4a12e6c7b5e131aa
rahul singh <rohit.singh11299@gmail.com>
2024-06-29 18:07:36 +0530

    using express-validator

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -1,27 +1,34 @@
+import { body, validationResult } from "express-validator";
 // export default only works on
-// 1. Hoisted declaration => a function 
+// 1. Hoisted declaration => a function
 // 2. class
 // 3. assigned expression
-// XX export default const validateRequest = (req, res, next) => { 
-const validateRequest = (req, res, next) => {
-  const { name, price, imageUrl } = req.body;
-  let errors = [];
-  if (!name || name.trim() == "") {
-    errors.push("Name is required");
-  }
-  if (!price || parseFloat(price) < 1) {
-    errors.push("price must be a positive value");
-  }
-  try {
-    const url = new URL(imageUrl);
-  } catch (error) {
-    errors.push("URL is Invalid");
-  }
-  if (errors.length > 0) {
-    res.render("new-product", {
-      errorMessage: errors[0],
+// XX export default const validateRequest = (req, res, next) => {
+    const validateRequest = async (req, res, next) => {
+    console.log(req.body);
+  // 1. setup Rules for validation
+  const rules = [
+    body("name").isEmpty().withMessage("Name is required."),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price should be a Positive Integer."),
+    body("imageUrl").isURL().withMessage("Invalid URL"),
+  ];
+  
+  // 2. Run those Rules
+  await Promise.all(rules.map(rule => rule.run(req)));
+
+  // 3. check if there is any errors after running the rules
+  let validationErrors = validationResult(req);
+  console.log(validationErrors.array());
+
+  // 4 if errros, Return the error message
+  if(!validationErrors.isEmpty()){
+    return res.render('new-product', {
+        errorMessage: validationErrors.array()[0].msg
     });
   }
+
   next();
 };
 
